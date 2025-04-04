d74491be60813fd9400ee28ae729f6100b847590
김채림 <woqja0905@naver.com>
2025-04-04 14:14:20 +0900

    update:header map 수정

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,62 +1,62 @@
+import Image from "next/image";
+
 export default function Header() {
+  const headerMainMenus = [
+    { name: "MUSINSA", href: "#" },
+    { name: "BEAUTY", href: "#" },
+    { name: "PLAYER", href: "#" },
+    { name: "OUTLET", href: "#" },
+    { name: "BOUTIQUE", href: "#" },
+    { name: "SNEAKERS", href: "#" },
+    { name: "KIDS", href: "#" },
+  ];
+
+  const headerSubMenus = [
+    { name: "검색", href: "#" },
+    { name: "좋아요", href: "#" },
+    { name: "마이", href: "#" },
+    { name: "장바구니", href: "#" },
+  ];
   return (
     <>
-      <div className="w-full h-[55px] bg-[#1A1B1F] py-[20px] x-[8px] px-[8px]">
-        <div className="flex items-center gap-[8px] w-[1440px] h-[16px] p-[8px]">
-          <button>
-            <img
-              src="/navIcon.svg"
-              alt="navIcon"
-              className="w-[20px] h-[15px]"
-            />
-          </button>
-          <div className="pl-[8px]">
+      <div className="w-full h-[55px] bg-[#1A1B1F] py-[20px] px-[8px]">
+        <div className="flex items-center justify-between gap-[8px] h-[16px] px-[8px]">
+          <div className="flex items-center gap-[8px]">
+            <button className="cursor-pointer">
+              <Image src="/navIcon.svg" alt="navIcon" width={20} height={15} />
+            </button>
+            <div>
+              <div className="w-[1px] h-[16px] bg-[#767679] ml-[8px]" />
+            </div>
+            {headerMainMenus.map((menu, index) => (
+              <span
+                key={index}
+                className="text-[#D1D1D2] font-semibold cursor-pointer"
+              >
+                <a href={menu.href}>{menu.name}</a>
+              </span>
+            ))}
             <div className="w-[1px] h-[16px] bg-[#767679]" />
+            <span className="text-[#D1D1D2] font-semibold cursor-pointer">
+              <a href="#">
+                <span className="font-light">(S)</span>SNAP
+              </a>
+            </span>
           </div>
-          <span className="text-[#D1D1D2] font-semibold cursor-pointer">
-            <a href="#">MUSINSA</a>
-          </span>
-          <span className="text-[#D1D1D2] font-semibold cursor-pointer">
-            <a href="#">BEAUTY</a>
-          </span>
-          <span className="text-[#D1D1D2] font-semibold cursor-pointer">
-            <a href="#">PLAYER</a>
-          </span>
-          <span className="text-[#D1D1D2] font-semibold cursor-pointer">
-            <a href="#">OUTLET</a>
-          </span>
-          <span className="text-[#D1D1D2] font-semibold cursor-pointer">
-            <a href="#">BOUTIQUE</a>
-          </span>
-          <span className="text-[#D1D1D2] font-semibold cursor-pointer">
-            <a href="#">SNEAKERS</a>
-          </span>
-          <span className="text-[#D1D1D2] font-semibold cursor-pointer">
-            <a href="#">KIDS</a>
-          </span>
-          <div className="w-[1px] h-[16px] bg-[#767679]" />
-          <span className="text-[#D1D1D2] font-semibold cursor-pointer">
-            <a href="#">
-              <span className="font-light">(S)</span>SNAP
-            </a>
-          </span>
-          <div className="flex items-end  gap-[8px]">
+
+          <div className="flex items-center  gap-[8px] ">
             <span className="text-[#D1D1D2] text-[14px] font-semibold cursor-pointer">
               <a href="#">오프라인 스토어</a>
             </span>
             <div className="w-[1px] h-[16px] bg-[#767679]" />
-            <span className="text-[#D1D1D2] text-[14px] font-semibold cursor-pointer">
-              <a href="#">검색</a>
-            </span>
-            <span className="text-[#D1D1D2] text-[14px] font-semibold cursor-pointer">
-              <a href="#">좋아요</a>
-            </span>
-            <span className="text-[#D1D1D2] text-[14px] font-semibold cursor-pointer">
-              <a href="#">마이</a>
-            </span>
-            <span className="text-[#D1D1D2] text-[14px] font-semibold cursor-pointer">
-              <a href="#">장바구니</a>
-            </span>
+            {headerSubMenus.map((menu, index) => (
+              <span
+                key={index}
+                className="text-[#D1D1D2] text-[14px] font-semibold cursor-pointer"
+              >
+                <a href={menu.href}>{menu.name}</a>
+              </span>
+            ))}
           </div>
         </div>
       </div>
