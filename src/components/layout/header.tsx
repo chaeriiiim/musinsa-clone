ba9ee1a0f444691e2f4b260e617a28beb93004fd
김채림 <woqja0905@naver.com>
2025-04-04 11:03:10 +0900

    fix: globals.css / layout.tsx css 수정

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <div className="w-[1440px] h-[192px] bg-[#1A1B1F] py-20px px-16px">
+      <div className="w-full h-[192px] bg-[#1A1B1F] py-[20px] px-[16px]">
         <p className="text-white">헤더입니다!</p>
       </div>
     </>
